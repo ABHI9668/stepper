b41333884d6f5c5f6a929b7b3594487aa48cc627
Abhishek Singh <as6867909@gmail.com>
2024-02-22 22:50:23 +0530

    stepper-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Stepperr from './components/Stepperr';
+
+
+const CHECKOUT_STEPS=[
+  {
+    name :"Customer Info",
+    Component:()=> <div><h3>Provide your contact details</h3></div>
+},
+{
+  name :"Shipping Info",
+  Component:()=> <div><h3>Enter your Shipping address</h3></div>
+},
+{
+  name :"Payment",
+  Component:()=> <div><h3>Complete payment for your order</h3></div>
+},
+{
+  name :"Delivered",
+  Component:()=> <div><h3>Your Order has been Delivered</h3></div>
+}
+]
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h2 style={{marginLeft:"20px"}}>Checkout</h2>
+    <Stepperr stepsConfig={CHECKOUT_STEPS}/>
     </div>
   );
 }
